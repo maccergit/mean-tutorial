1169afad1cb319299ae69c4a5c3d8ae1d6d8e3b5
John McAlister <john.mcalister@ricoh-usa.com>
2016-05-09 18:51:52 -0600

    Force view engine to "pug", add repo to package, clean up main view

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ var parser = require('body-parser');
 // Create the application object
 var myapp = express();
 
+// Tell the app where the views live, and what engine should render them.
+myapp.set('views', './views');
+myapp.set('view engine', 'pug');
+
+// Tell the app to use the static server to serve files in the CSS directory.
 myapp.use(express.static('css'));
 
+// Tell the app about the main view.
 myapp.route('/', function(req, res) {
   res.render('index.pug');
 });
 
+// Tell the app how to render views from links.
 myapp.use(function(req, res, next) {
   res.render(req.path.slice(1));
 });
